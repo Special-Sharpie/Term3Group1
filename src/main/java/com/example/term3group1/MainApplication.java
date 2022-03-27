245c44a40fb5bb30cbc76870bcb88b5bc50a2f26
rich-cook <richardtcook@gmail.com>
2022-03-26 22:21:08 -0600

    created the main form, and downloaded the my sql library
    mysql:mysql-connector-java:8.0.27 downloaded, as well, button click handle events were generated (but empty)

--- src/main/java/com/example/term3group1/MainApplication.java
+++ src/main/java/com/example/term3group1/MainApplication.java
@@ -11,8 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 620, 540);
+        stage.setTitle("Travel Experts");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/term3group1/MainController.java
+++ src/main/java/com/example/term3group1/MainController.java
@@ -1,14 +1,76 @@
 package com.example.term3group1;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnAgents;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnProducts;
+
+    @FXML
+    private Button btnSuppliers;
+
+    @FXML
+    void initialize() {
+        assert btnAgents != null : "fx:id=\"btnAgents\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert btnPackages != null : "fx:id=\"btnPackages\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert btnProducts != null : "fx:id=\"btnProducts\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert btnSuppliers != null : "fx:id=\"btnSuppliers\" was not injected: check your FXML file 'main-view.fxml'.";
+
+        //initiate the agents form
+        btnAgents.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+            }
+        });
+
+        //initiate the packages form
+        btnPackages.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+            }
+        });
+
+
+        //initiate the products from
+
+        btnProducts.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+            }
+        });
+
+
+        //initiate the suppliers form
+
+        btnSuppliers.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+
+            }
+        });
+
     }
+
 }
